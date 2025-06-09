e65b7618f9be0979440e09a367becd721d2d8b7a
Harish123540 <hbhatt41@gmail.com>
2025-06-09 16:25:45 +0530

    Update my-cdk-wordpress.ts

--- bin/my-cdk-wordpress.ts
+++ bin/my-cdk-wordpress.ts
@@ -11,7 +11,7 @@ new MyCdkWordpressStack(app, 'MyCdkWordpressStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
- env: { account: '739275466771', region: 'ap-south-1' },
+ env: { account: '975826764450', region: 'ap-south-1' },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
